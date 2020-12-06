202998537dd86a00028151806de5d2d29514cd16
leogtzr <leogutierrezramirez@gmail.com>
2020-12-05 19:15:40 -0700

    remove unnecessary function

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
 	"github.com/go-redis/redis"
@@ -14,26 +13,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-
-// Render one of HTML, JSON or CSV based on the 'Accept' header of the request
-// If the header doesn't specify this, HTML is rendered, provided that
-// the template name is present
-func render2(c *gin.Context, data gin.H, templateName string) {
-	loggedInInterface, _ := c.Get("is_logged_in")
-	data["is_logged_in"] = loggedInInterface.(bool)
-
-	switch c.Request.Header.Get("Accept") {
-	case "application/json":
-		// Respond with JSON
-		c.JSON(http.StatusOK, data["payload"])
-	case "application/xml":
-		// Respond with XML
-		c.XML(http.StatusOK, data["payload"])
-	default:
-		// Respond with HTML
-		c.HTML(http.StatusOK, templateName, data)
-	}
-}
 
 func init() {
 	var err error
